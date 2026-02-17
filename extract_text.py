f1b06f1f133a8f052e6a1ebad8a1bef4434acfe4
kallel763 <mkallel170@gmail.com>
2026-02-17 11:06:58 +0100

    modify1

--- extract_text.py
+++ extract_text.py
@@ -2,7 +2,7 @@ import pdfplumber
 
 text = ""
 
-with pdfplumber.open("law.pdf") as pdf:
+with pdfplumber.open("law19.pdf") as pdf:
     for page in pdf.pages:
         text += page.extract_text() + "\n"
 
